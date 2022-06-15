3336fb6ed13518ae9f068d174f1598310b6df0a3
Juan <jgarcia1545@gmail.com>
2022-06-15 15:23:21 -0700

    Added more initial files and began creating the models

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        }
+    }
+);
+
+const User = model(UserSchema);
+
+module.exports = User;
